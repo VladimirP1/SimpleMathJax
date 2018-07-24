f2313709bebfecf41a574e619fb408e7bed2220c
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2018-07-24 16:14:23 +0300

    Added color support

--- modules/ext.SmjCDN.js
+++ modules/ext.SmjCDN.js
@@ -1,6 +1,6 @@
 $.getScript( '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js',
 	function () {
-		var extensions = ["tex2jax.js","TeX/AMSmath.js"];
+		var extensions = ["tex2jax.js","TeX/AMSmath.js","TeX/color.js","TeX/AMSmath.js","TeX/AMSsymbols.js","TeX/noErrors.js","TeX/noUndefined.js"];
 		if( mw.config.get('wgSmjUseChem') ) extensions.push("TeX/mhchem.js");
 		MathJax.Hub.Config({
 			messageStyle: "none",
